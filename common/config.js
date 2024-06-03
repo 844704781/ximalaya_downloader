f76f8468a6acfeee62746ffab010ce8e0cd9d8f7
hamlin.li <sclihuiming@163.com>
2024-06-04 00:00:25 +0800

    perf: use the default config when the root config file does not exist

--- common/config.js
+++ common/config.js
@@ -1,7 +1,11 @@
 import fs from "fs";
 import {projectRoot} from "../settings.js";
+import defaultConfig from "../config.json";
 
-const configBuf = fs.readFileSync(`${projectRoot}/config.json`)
-const _config = JSON.parse(String(configBuf))
+let _config = defaultConfig
+if (fs.existsSync(`${projectRoot}/config.json`)) {
+  const configBuf = fs.readFileSync(`${projectRoot}/config.json`)
+  _config = JSON.parse(String(configBuf))
+}
 
 export const config = _config
