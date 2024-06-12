c222cc9dd6f0ba9e98490f1d6344b8a9d520b1ce
嬗衍 <shanyanwcx@qq.com>
2024-06-12 20:45:45 +0800

    Modify default config

--- common/config.js
+++ common/config.js
@@ -1,7 +1,22 @@
 import fs from "fs";
 import {projectRoot} from "../settings.js";
 
-let _config
+let _config = {
+  "baseUrl": "https://www.ximalaya.com",
+  "loginBaseUrl": "https://passport.ximalaya.com",
+  "archives": "~/Downloads",
+  "xmd": "~/.xmd",
+  "cookie": {
+    "www2": {
+      "serverMode": false,
+      "value": ""
+    },
+    "mac": {
+      "serverMode": false,
+      "value": ""
+    }
+  }
+}
 if (fs.existsSync(`${projectRoot}/config.json`)) {
   const configBuf = fs.readFileSync(`${projectRoot}/config.json`)
   _config = JSON.parse(String(configBuf))
